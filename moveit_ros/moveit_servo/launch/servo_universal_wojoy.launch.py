84908b4c75cf5cddba51949c74217bd06dc514bb
Declan Shanahan <d.shanahan1@lancaster.ac.uk>
2025-06-17 17:40:56 +0100

    no idea some changes to servo

---
+++ moveit_ros/moveit_servo/launch/servo_universal_wojoy.launch.py
@@ -0,0 +1,368 @@
+import yaml
+import xacro
+import os
+from ur_moveit_config.launch_common import load_yaml
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.conditions import IfCondition
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, ExecuteProcess
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.descriptions import ComposableNode
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+
+
+
+# def load_file(package_name, file_path):
+#     package_path = get_package_share_directory(package_name)
+#     absolute_file_path = os.path.join(package_path, file_path)
+
+#     try:
+#         with open(absolute_file_path, "r") as file:
+#             return file.read()
+#     except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+#         return None
+
+
+# def load_yaml(package_name, file_path):
+#     package_path = get_package_share_directory(package_name)
+#     absolute_file_path = os.path.join(package_path, file_path)
+
+#     try:
+#         with open(absolute_file_path, "r") as file:
+#             return yaml.safe_load(file)
+#     except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+#         return None
+
+
+def generate_launch_description():
+
+        # Hardcode some arguments
+    moveit_config_package = "ur_moveit_config"
+    description_package = "ur_description"
+    description_file = "ur.urdf.xacro"
+    ur_type = "ur5e" # "ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20", "ur30"
+    moveit_config_file = "ur.srdf.xacro"
+    # Initialize Arguments
+    safety_limits = LaunchConfiguration("safety_limits")
+    safety_pos_margin = LaunchConfiguration("safety_pos_margin")
+    safety_k_position = LaunchConfiguration("safety_k_position")
+    # General arguments
+    _publish_robot_description_semantic = LaunchConfiguration("publish_robot_description_semantic")
+    moveit_joint_limits_file = LaunchConfiguration("moveit_joint_limits_file")
+    warehouse_sqlite_path = LaunchConfiguration("warehouse_sqlite_path")
+    prefix = LaunchConfiguration("prefix")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    launch_rviz = LaunchConfiguration("launch_rviz")
+    launch_servo = LaunchConfiguration("launch_servo")
+
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
+            " ",
+            "robot_ip:=xxx.yyy.zzz.www",
+            " ",
+            "joint_limit_params:=",
+            PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "joint_limits.yaml"]
+    ),
+            " ",
+            "kinematics_params:=",
+            PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "default_kinematics.yaml"]
+    ),
+            " ",
+            "physical_params:=",
+            PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "physical_parameters.yaml"]
+    ),
+            " ",
+            "visual_params:=",
+            PathJoinSubstitution(
+        [FindPackageShare(description_package), "config", ur_type, "visual_parameters.yaml"]
+            ),
+            " ",
+            "safety_limits:=",
+            safety_limits,
+            " ",
+            "safety_pos_margin:=",
+            safety_pos_margin,
+            " ",
+            "safety_k_position:=",
+            safety_k_position,
+            " ",
+            "name:=",
+            "ur",
+            " ",
+            "ur_type:=",
+            ur_type,
+            " ",
+            "script_filename:=ros_control.urscript",
+            " ",
+            "input_recipe_filename:=rtde_input_recipe.txt",
+            " ",
+            "output_recipe_filename:=rtde_output_recipe.txt",
+            " ",
+            "prefix:=",
+            prefix,
+            " ",
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+    controllers_yaml = load_yaml("ur_moveit_config", "config/controllers.yaml")
+
+
+    # description_package = "ur_description"
+    # description_file = "ur.urdf.xacro"
+    # robot_description_content = Command(
+    #     [
+    #         PathJoinSubstitution([FindExecutable(name="xacro")]),
+    #         " ",
+    #         PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
+    #         " ",
+    #         "safety_limits:=",
+    #         "true",
+    #         " ",
+    #         "safety_pos_margin:=",
+    #         "0.15",
+    #         " ",
+    #         "safety_k_position:=",
+    #         "20",
+    #         " ",
+    #         "name:=",
+    #         "ur",
+    #         " ",
+    #         "ur_type:=",
+    #         "ur10e",
+    #         " ",
+    #         "tf_prefix:=",
+    #          '""',
+    #     ]
+    # )
+    
+    # moveit_config = (
+    #     MoveItConfigsBuilder("ur")
+    #     .robot_description == robot_description_content
+    #     .to_moveit_configs()
+    # )
+
+
+    # Get parameters for the Servo node
+    servo_yaml = load_yaml("moveit_servo", "config/ur_simulated_config.yaml")
+    servo_params = {"moveit_servo": servo_yaml}
+
+
+    # MoveIt Configuration
+    robot_description_semantic_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(moveit_config_package), "srdf", moveit_config_file]
+            ),
+            " ",
+            "name:=",
+            # Also ur_type parameter could be used but then the planning group names in yaml
+            # configs has to be updated!
+            "ur",
+            " ",
+            "prefix:=",
+            prefix,
+            " ",
+        ]
+    )
+
+    robot_description_semantic = {"robot_description_semantic": robot_description_semantic_content}
+
+    robot_description_kinematics = PathJoinSubstitution(
+        [FindPackageShare(moveit_config_package), "config", "kinematics.yaml"]
+    )
+
+    robot_description_planning = {
+        "robot_description_planning": load_yaml("ur_moveit_config", "config/joint_limits.yaml"
+        )
+    }
+
+
+    # RViz
+    # rviz_config_file = (
+    #     get_package_share_directory("moveit_servo") + "/config/demo_rviz_config.rviz"
+    # )
+    # rviz_node = Node(
+    #     package="rviz2",
+    #     executable="rviz2",
+    #     name="rviz2",
+    #     output="log",
+    #     arguments=["-d", rviz_config_file],
+    #     parameters=[
+    #         moveit_config.robot_description,
+    #         moveit_config.robot_description_semantic,
+    #     ],
+    # )
+
+    # # ros2_control using FakeSystem as hardware
+    # ros2_controllers_path = os.path.join(
+    #     get_package_share_directory("ur_moveit_config"),
+    #     "config",
+    #     "controllers.yaml",
+    # )
+    # ros2_control_node = Node(
+    #     package="controller_manager",
+    #     executable="ros2_control_node",
+    #     parameters=[robot_description, controllers_yaml],
+    #     output="screen",
+    # )
+
+    # joint_state_broadcaster_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=[
+    #         "joint_state_broadcaster",
+    #         "--controller-manager-timeout",
+    #         "300",
+    #         "--controller-manager",
+    #         "/controller_manager",
+    #     ],
+    # )
+
+    # ur_arm_controller_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["ur_arm_controller", "-c", "/controller_manager"],
+    # )
+
+    # Launch as much as possible in components
+    container = ComposableNodeContainer(
+        name="moveit_servo_demo_container",
+        namespace="/",
+        package="rclcpp_components",
+        executable="component_container_mt",
+        composable_node_descriptions=[
+            # Example of launching Servo as a node component
+            # Assuming ROS2 intraprocess communications works well, this is a more efficient way.
+            # ComposableNode(
+            #     package="moveit_servo",
+            #     plugin="moveit_servo::ServoServer",
+            #     name="servo_server",
+            #     parameters=[
+            #         servo_params,
+            #         moveit_config.robot_description,
+            #         moveit_config.robot_description_semantic,
+            #     ],
+            # ),
+            ComposableNode(
+                package="robot_state_publisher",
+                plugin="robot_state_publisher::RobotStatePublisher",
+                name="robot_state_publisher",
+                parameters=[robot_description],
+            ),
+            ComposableNode(
+                package="tf2_ros",
+                plugin="tf2_ros::StaticTransformBroadcasterNode",
+                name="static_tf2_broadcaster",
+                parameters=[{"child_frame_id": "/base_link", "frame_id": "/world"}],
+            ),
+            ComposableNode(
+                package="moveit_servo",
+                plugin="moveit_servo::JoyToServoPub",
+                name="controller_to_servo_node",
+            ),
+        ],
+        output="screen",
+    )
+    # Launch a standalone Servo node.
+    # As opposed to a node component, this may be necessary (for example) if Servo is running on a different PC
+    servo_node = Node(
+        package="moveit_servo",
+        executable="servo_node_main",
+        parameters=[
+            servo_params,
+            robot_description,
+            robot_description_semantic,
+            robot_description_kinematics,
+        ],
+        output="screen",
+    )
+
+    declared_arguments = []
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_limits",
+            default_value="true",
+            description="Enables the safety limits controller if true.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_pos_margin",
+            default_value="0.15",
+            description="The margin to lower and upper limits in the safety controller.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "safety_k_position",
+            default_value="20",
+            description="k-position factor in the safety controller.",
+        )
+    )
+    # General arguments
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "publish_robot_description_semantic",
+            default_value="True",
+            description="Whether to publish the SRDF description on topic /robot_description_semantic.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "moveit_joint_limits_file",
+            default_value="joint_limits.yaml",
+            description="MoveIt joint limits that augment or override the values from the URDF robot_description.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "warehouse_sqlite_path",
+            default_value=os.path.expanduser("~/.ros/warehouse_ros.sqlite"),
+            description="Path where the warehouse database should be stored",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="false",
+            description="Make MoveIt to use simulation time. This is needed for the trajectory planing in simulation.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "prefix",
+            default_value='""',
+            description="Prefix of the joint names, useful for "
+            "multi-robot setup. If changed than also joint names in the controllers' configuration "
+            "have to be updated.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument("launch_servo", default_value="true", description="Launch Servo?")
+    )
+
+    return LaunchDescription( declared_arguments +
+        [
+            # ros2_control_node,
+            # joint_state_broadcaster_spawner,
+            # ur_arm_controller_spawner,
+            servo_node,
+            container,
+        ]
+    )
